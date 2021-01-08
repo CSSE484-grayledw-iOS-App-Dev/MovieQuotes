652c2fb81f7642c8888369fe86cfec5b7ad1e750
Derek Grayless <grayledw@rose-hulman.edu>
2021-01-07 20:21:36 -0500

    Work through 21:16 of first video

---
+++ MovieQuotes/TempViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TempViewController.swift
+//  MovieQuotes
+//
+//  Created by Derek Grayless on 1/7/21.
+//
+
+import Foundation
+
+import UIKit
+
+class TempViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    let tempCellIdentifier = "TempCell";
+}

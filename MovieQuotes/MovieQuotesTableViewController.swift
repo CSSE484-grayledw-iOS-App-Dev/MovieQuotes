3023679620347c881b47db86ca1c301a7e25ebba
Derek Grayless <grayledw@rose-hulman.edu>
2021-01-16 14:04:17 -0500

    added create new quote via dialog

--- MovieQuotes/MovieQuotesTableViewController.swift
+++ MovieQuotes/MovieQuotesTableViewController.swift
@@ -23,7 +23,34 @@ class MovieQuotesTableViewController: UITableViewController {
     }
     
     @objc func showAddQuoteDialog() {
-        print("You pressed the add button")
+        // Add a dialog -> dialogs are all about crud
+        
+        let alertController = UIAlertController(title: "Create a new movie quote", message: "", preferredStyle: .alert)
+        
+        // Configure
+        alertController.addTextField(configurationHandler: { (textField) in
+            textField.placeholder = "Quote"
+        })
+        
+        alertController.addTextField(configurationHandler: { (textField) in
+            textField.placeholder = "Movie"
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        let submitAction = UIAlertAction(title: "Create Quote", style: .default) { (action) in
+            let quoteTextField = alertController.textFields![0] as UITextField
+            let movieTextField = alertController.textFields![1] as UITextField
+//            print(quoteTextField.text!)
+//            print(movieTextField.text!)
+            let newMovieQuote = MovieQuote(quote: quoteTextField.text!, movie: movieTextField.text!)
+            self.movieQuotes.insert(newMovieQuote, at: 0)
+            self.tableView.reloadData()
+        }
+        alertController.addAction(submitAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {

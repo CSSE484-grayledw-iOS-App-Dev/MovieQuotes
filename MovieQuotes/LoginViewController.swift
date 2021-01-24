786fffb416c4121b420de02fa0f720e3c848a593
Derek Grayless <grayledw@rose-hulman.edu>
2021-01-23 22:40:48 -0500

    sign in and log in via Email password works - 2nd commit of email password video

--- MovieQuotes/LoginViewController.swift
+++ MovieQuotes/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
     let ShowListSegueIdentifier = "ShowListSegue"
@@ -13,12 +14,43 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
-    
-    
-    @IBAction func pressedSignInNewUser(_ sender: Any) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        emailTextField.placeholder = "Email"
+        passwordTextField.placeholder = "Password"
     }
     
     
+    @IBAction func pressedSignInNewUser(_ sender: Any) {
+        let email = emailTextField.text!
+        let password = passwordTextField.text!
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+            if let error = error {
+                print("Error creating a new user for Email/Password \(error)")
+                return
+            }
+            
+            print("It worked! A new user is created and now signed in!")
+            print("Email \(authResult!.user.email!) UID: \(authResult!.user.uid)")
+            self.performSegue(withIdentifier: self.ShowListSegueIdentifier, sender: self)
+        }
+    }
+
     @IBAction func pressedLogInExistingUser(_ sender: Any) {
+        
+        let email = emailTextField.text!
+        let password = passwordTextField.text!
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
+            if let error = error {
+                print("Error logging in an existing user for Email/Password \(error)")
+                return
+            }
+            
+            print("It worked! A user has logged in!")
+            print("Email \(authResult!.user.email!) UID: \(authResult!.user.uid)")
+            self.performSegue(withIdentifier: self.ShowListSegueIdentifier, sender: self)
+        }
     }
 }

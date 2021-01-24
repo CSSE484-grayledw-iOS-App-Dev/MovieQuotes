bd34d1428bfb3c003ee72644269a60c96ef4473e
Derek Grayless <grayledw@rose-hulman.edu>
2021-01-24 14:34:12 -0500

    show all quotes and my quotes - 2nd commit of side nav view controller video

--- MovieQuotes/MovieQuotesTableViewController.swift
+++ MovieQuotes/MovieQuotesTableViewController.swift
@@ -22,53 +22,53 @@ class MovieQuotesTableViewController: UITableViewController {
         super.viewDidLoad()
         
         // navigationItem.leftBarButtonItem = editButtonItem
-//        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
-//                                                            target: self,
-//                                                            action: #selector(showAddQuoteDialog))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
+                                                            target: self,
+                                                            action: #selector(showAddQuoteDialog))
         
 //        movieQuotes.append(MovieQuote(quote: "I'll be back", movie: "The Terminator"))
 //        movieQuotes.append(MovieQuote(quote: "Yo Adrian!", movie: "Rocky"))
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Menu", style: .plain, target: self, action: #selector(showMenu))
+        // navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Menu", style: .plain, target: self, action: #selector(showMenu))
         
         movieQuotesRef = Firestore.firestore().collection("MovieQuotes")
     }
     
-    @objc func showMenu() {
-        // Add a dialog -> dialogs are all about crud
-        
-        let alertController = UIAlertController(title: nil, message: "", preferredStyle: .actionSheet)
-        
-        let submitAction = UIAlertAction(title: "Create Quote", style: .default) { (action) in
-            self.showAddQuoteDialog()
-        }
-        
-        let showAction = UIAlertAction(title: self.isShowingAllQuotes ? "Show only my quotes" : "Show all quotes", style: .default) { (action) in
-            // Toggle between the show all vs show mine mode
-            self.isShowingAllQuotes = !self.isShowingAllQuotes
-            // Update the list
-            self.startListening()
-        }
-        
-        let signOutAction = UIAlertAction(title: "Sign Out", style: .default) { (action) in
-            do {
-                try Auth.auth().signOut()
-                } catch {
-                    print("Sign out error")
-                }
-            
-        }
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        alertController.addAction(cancelAction)
-        alertController.addAction(showAction)
-        alertController.addAction(signOutAction)
-        alertController.addAction(submitAction)
-        
-        present(alertController, animated: true, completion: nil)
-    }
+//    @objc func showMenu() {
+//        // Add a dialog -> dialogs are all about crud
+//
+//        let alertController = UIAlertController(title: nil, message: "", preferredStyle: .actionSheet)
+//
+//        let submitAction = UIAlertAction(title: "Create Quote", style: .default) { (action) in
+//            self.showAddQuoteDialog()
+//        }
+//
+//        let showAction = UIAlertAction(title: self.isShowingAllQuotes ? "Show only my quotes" : "Show all quotes", style: .default) { (action) in
+//            // Toggle between the show all vs show mine mode
+//            self.isShowingAllQuotes = !self.isShowingAllQuotes
+//            // Update the list
+//            self.startListening()
+//        }
+//
+//        let signOutAction = UIAlertAction(title: "Sign Out", style: .default) { (action) in
+//            do {
+//                try Auth.auth().signOut()
+//                } catch {
+//                    print("Sign out error")
+//                }
+//
+//        }
+//
+//        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+//        alertController.addAction(cancelAction)
+//        alertController.addAction(showAction)
+//        alertController.addAction(signOutAction)
+//        alertController.addAction(submitAction)
+//
+//        present(alertController, animated: true, completion: nil)
+//    }
 
-    func showAddQuoteDialog() {
+    @objc func showAddQuoteDialog() {
         // Add a dialog -> dialogs are all about crud
         
         let alertController = UIAlertController(title: "Create a new movie quote", message: "", preferredStyle: .alert)
--- MovieQuotes/SideNavViewController.swift
+++ MovieQuotes/SideNavViewController.swift
@@ -10,4 +10,43 @@ import UIKit
 class SideNavViewController : UIViewController {
     
     
+    @IBAction func pressedGoToProfilePage(_ sender: Any) {
+        print("Profile")
+    }
+    
+    
+    @IBAction func pressedShowAllQuotes(_ sender: Any) {
+//        print("All Quotes")
+//        print("Presenting view controller is \(presentingViewController)") --> is the navigationController
+        
+        tableViewController.isShowingAllQuotes = true
+        tableViewController.startListening()
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func pressedShowMyQuotes(_ sender: Any) {
+        print("My Quotes")
+        tableViewController.isShowingAllQuotes = false
+        tableViewController.startListening()
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func pressedDeleteQuotes(_ sender: Any) {
+        print("Delete Quotes")
+    }
+    
+    
+    @IBAction func pressedSignOut(_ sender: Any) {
+        print("Sign Out")
+    }
+    
+    var tableViewController: MovieQuotesTableViewController {
+        // don't need to specify get below but I like it there
+        get {
+            let navController = presentingViewController as! UINavigationController
+            return navController.viewControllers.last as! MovieQuotesTableViewController
+        }
+    }
 }

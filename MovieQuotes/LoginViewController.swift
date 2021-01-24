c6761a2b5a29e72ff65e9c145e427357bdd96158
Derek Grayless <grayledw@rose-hulman.edu>
2021-01-23 22:26:50 -0500

    ui for email password - 1st commit email password video

---
+++ MovieQuotes/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  MovieQuotes
+//
+//  Created by Derek Grayless on 1/23/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    let ShowListSegueIdentifier = "ShowListSegue"
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    @IBAction func pressedSignInNewUser(_ sender: Any) {
+    }
+    
+    
+    @IBAction func pressedLogInExistingUser(_ sender: Any) {
+    }
+}

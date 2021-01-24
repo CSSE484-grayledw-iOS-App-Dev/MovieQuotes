de09f40e59803d28672f5bc225b776c6fa45d899
Derek Grayless <grayledw@rose-hulman.edu>
2021-01-24 12:27:28 -0500

    added new root controller to side nav - first commit of side nav view controller video

---
+++ MovieQuotes/SideNavViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SideNavViewController.swift
+//  MovieQuotes
+//
+//  Created by Derek Grayless on 1/24/21.
+//
+
+import UIKit
+
+class SideNavViewController : UIViewController {
+    
+    
+}
